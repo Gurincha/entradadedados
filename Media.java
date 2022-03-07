cbcc71d80269fc9e28916d7912cb2596627a4dbd
Gurincha <gustavo.rincha@fatec.sp.gov.br>
2022-03-07 17:20:01 +0000

    salvo

---
+++ Media.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Media{
+    
+    public static void main(String[] args) {
+        
+        double numero1;
+        double numero2;
+        double media;
+
+        Scanner leitura=new Scanner(System.in);
+        
+        System.out.println("Digite uma nota: ");
+        numero1 = leitura.nextDouble();
+        System.out.println("Digite a outra nota: ");
+        numero2 = leitura.nextDouble();
+        media = (numero1 + numero2)/2;
+
+        System.out.printf("Sua media foi de: %.2f\n", media);
+      
+        if(media>=6){
+            System.out.print("voce passou ");
+        }else {
+            System.out.print("voce não passou ");
+        }
+    }
+}
